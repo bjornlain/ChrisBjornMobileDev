6bc47787227c743b6ffc7efeab8ab1a78600d041
bjornlain <11500443@student.pxl.be>
2018-10-17 21:27:47 +0200

    google maps gefixed + bijna restauranten als markers in de map

--- app/src/main/java/com/example/teambc/projectmobiledevelop/MainActivity.java
+++ app/src/main/java/com/example/teambc/projectmobiledevelop/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.teambc.projectmobiledevelop;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +15,10 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity  {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+public class MainActivity extends AppCompatActivity {
 
     /* TextView mRestaurantTextView;
      Button mMenuButton;
@@ -24,6 +29,9 @@ public class MainActivity  {
     LinearLayout mInsideLinear;
     Button terugButton;
     Button sorteerButton;
+    Button mapsButton;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,9 +49,11 @@ public class MainActivity  {
 
         for(int i = 0; i < dummyNames.length;i++){
             LinearLayout linear = new LinearLayout(this);
-            linear.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,230));
+            LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            linearLayoutParams.weight = 20;
             linear.setOrientation(LinearLayout.HORIZONTAL);
             linear.setPadding(10,0,0,0);
+            linear.setLayoutParams(linearLayoutParams);
             TextView restName = new TextView(this);
             restName.setText(dummyNames[i]);
             restName.setTextSize(18);
@@ -112,6 +122,9 @@ public class MainActivity  {
 
         }
         configureerButtons();
+        if(isServicesOK()){
+        init();
+        }
     }
     private void configureerButtons(){
         sorteerButton.setOnClickListener(new View.OnClickListener() {
@@ -121,4 +134,34 @@ public class MainActivity  {
             }
         });
     }
+
+    private void init(){
+        mapsButton = findViewById(R.id.maps_button);
+        mapsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,googleActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean isServicesOK(){
+        Log.d("MainActivity", "isServicesOK : checking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if (available == ConnectionResult.SUCCESS){
+            Log.d("MainActivity","isServicesOK()) : Google play services si working");
+            return true;
+        }else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //een error is gebeurt maar we kunnen het fixen
+            Log.d("MainActivity", "an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this,"we cant me map requests",Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+
 }
--- app/src/main/java/com/example/teambc/projectmobiledevelop/SorteerActivity.java
+++ app/src/main/java/com/example/teambc/projectmobiledevelop/SorteerActivity.java
@@ -8,12 +8,18 @@ import android.widget.Button;
 public class SorteerActivity extends AppCompatActivity {
 
     Button terugButton;
+    Button geopendButton;
+    Button ratingButton;
+    Button afstandButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sorteer_scherm);
 
         terugButton = (Button) findViewById(R.id.terug_button);
+        afstandButton = (Button) findViewById(R.id.afstand_button);
+        ratingButton = (Button) findViewById(R.id.rating_button);
+        geopendButton = (Button) findViewById(R.id.geopend_button);
         terugButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
